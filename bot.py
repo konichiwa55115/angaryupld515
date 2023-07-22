90b122eda6db8cc02208e579d653c5857a4f2071
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-22 19:30:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,9 @@
 import os
 from pyrogram import Client, filters
 from pyrogram.types import ForceReply
-import subprocess
 import shutil
 from os import system as cmd
 from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton , ReplyKeyboardMarkup , CallbackQuery
-from yt_dlp import YoutubeDL
-
-CHOOSE_UR_LANG = " Choose Your folmula ! "
-CHOOSE_UR_LANG_BUTTONS = [
-    [InlineKeyboardButton("vid 360p",callback_data="vid 360p")],
-     [InlineKeyboardButton("vid 720p",callback_data="vid 720p")],
-     [InlineKeyboardButton("aud",callback_data="aud")]
-]
 
 
 bot = Client(
@@ -24,10 +15,10 @@ bot = Client(
 @bot.on_message(filters.command('dl') & filters.private)
 def command1(bot,message):
     bot.send_message(message.chat.id, " جار التحميل  ",disable_web_page_preview=True)
-    zaza = 12
-    while (zaza <= 49): 
+    zaza = 18
+    while (zaza <= 75): 
      cmd(f'mkdir downloads')
-     cmd(f'cd ./downloads/ && wget https://al-angarie.com/audio/2-2/94/alebanh{zaza}.mp3 && cd ..' )
+     cmd(f'cd ./downloads/ && wget https://al-angarie.com/audio/Zm/88/1/NQ-ZM-0{zaza}.mp3 && cd ..' )
      zaza += 1
     cmd('uploadgram -1001821573758 ./downloads/ ')
     shutil.rmtree('./downloads/') 
